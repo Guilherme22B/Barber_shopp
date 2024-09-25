d1159a7d59569355df83b0a1097a492937b7f3f8
ChristianTezza <136117080+ChristianTezza@users.noreply.github.com>
2024-09-25 17:24:02 -0300

    Update home_page.dart

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class BarberShopsApp extends StatelessWidget {
-  const BarberShopsApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
