8d9ab9311126b301e5624476cad76f7e82ded040
carlos.angel.fr <carlos.angel.fr@gmail.com>
2024-12-08 06:10:03 +0000

    Adicionando um form na tela de login. Corrigindo alguns arquivos com problema de sintaxe

--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -3,7 +3,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:myapp/views/home/home_page.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  // GlobalKey para controlar o estado do formulário
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget loginForm(BuildContext context) {
     return Container(
@@ -16,84 +19,108 @@ class LoginPage extends StatelessWidget {
         ),
       ),
       width: double.infinity,
-      height: 306,
+      height: 320,
       alignment: Alignment.center,
       child: SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          const Text(
-            "Olá, Faça seu Login!",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 250,
-            height: 40,
-            child: TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'E-mail:',
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 12,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 250,
-            height: 40,
-            child: TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Senha',
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 12,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            "É novo aqui? Crie a sua conta!",
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 10),
-          SizedBox(
-            width: 265,
-            child: CupertinoButton(
-              color: const Color.fromRGBO(99, 47, 47, 1),
-              child: const Text(
-                "Confirmar",
+        child: Form(
+          key: _formKey, // Vinculando o formulário à chave global
+          child: Column(
+            children: [
+              const Text(
+                "Olá, Faça seu Login!",
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
                 ),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-              },
-            ),
+              const SizedBox(height: 20),
+              // Campo de e-mail
+              SizedBox(
+                width: 250,
+                height: 70,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'E-mail:',
+                    labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 12,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira o e-mail';
+                    }
+                    if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                      return 'E-mail inválido';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Campo de senha
+              SizedBox(
+                width: 250,
+                height: 70,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 12,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira a senha';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "É novo aqui? Crie a sua conta!",
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 10),
+              // Botão de envio
+              SizedBox(
+                width: 265,
+                child: CupertinoButton(
+                  color: const Color.fromRGBO(99, 47, 47, 1),
+                  child: const Text(
+                    "Confirmar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Se o formulário for válido, navegue para a página inicial
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-      ),
-      );
+    );
   }
 
   @override
@@ -112,26 +139,27 @@ class LoginPage extends StatelessWidget {
           ),
         ),
       ),
-      body:SingleChildScrollView( 
-      child:Container(
-        color: const Color(0xFF141518),
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.all(0.0),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 10),
-            Image.asset(
-              'assets/images/Barber-Logo.png',
-              height: 320,
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-              child: loginForm(context),
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color(0xFF141518),
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(0.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 10),
+              Image.asset(
+                'assets/images/Barber-Logo.png',
+                height: 200,
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                child: loginForm(context),
+              )
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
